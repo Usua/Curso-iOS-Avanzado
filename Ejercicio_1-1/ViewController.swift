2bdf13199a06026f850e13663d1eb0a1b5e422f8
usua <usua@OSX-iMac.local>
2015-06-19 13:28:49 +0200

    Initial Commit

---
+++ Ejercicio_1-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ejercicio_1-1
+//
+//  Created by usua on 19/6/15.
+//  Copyright (c) 2015 CysNET Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
